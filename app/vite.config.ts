e88ded7fe44f80425f66a26a47b652a4b980cdda
Cris <filipcristimihail@yahoo.com>
2024-03-26 16:02:42 +0100

    #3 Add Formik and Yup for Forms and Validation + Update MUI

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -2,7 +2,9 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mapMode.get(mode) ?? '', process.cwd(), 'VITE_')
+  const env = loadEnv(mode, process.cwd(), 'VITE_')
+
+  console.log(env)
 
   return {
     plugins: [react()],
@@ -11,7 +13,3 @@ export default defineConfig(({ mode }) => {
     }
   }
 })
-
-const mapMode = new Map<string, string>()
-mapMode.set('production', 'prod')
-mapMode.set('development', 'dev')
