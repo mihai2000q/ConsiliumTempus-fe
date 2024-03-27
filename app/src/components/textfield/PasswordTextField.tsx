e9291d0f2eb66a5aa7957753d93a02e424a20ba7
Cris <filipcristimihail@yahoo.com>
2024-03-27 09:43:55 +0100

    #3 Components - PasswordTextField - Update props

--- app/src/components/textfield/PasswordTextField.tsx
+++ app/src/components/textfield/PasswordTextField.tsx
@@ -1,13 +1,19 @@
 import { TextField } from "@mui/material";
+import React, { ReactNode } from "react";
 
 interface PasswordTextFieldProps {
+  name?: string,
   label?: string,
   placeholder?: string,
+  error?: boolean,
+  helperText?: ReactNode,
+  onBlur?: React.FocusEventHandler<HTMLTextAreaElement | HTMLInputElement>,
   value: string,
-  onValueChange: (password: string) => void
+  onChange: React.ChangeEventHandler<HTMLTextAreaElement | HTMLInputElement>
 }
 
 function PasswordTextField({
+  name = 'password',
   label = 'Password',
   placeholder = 'Enter your password',
   ...props
@@ -15,11 +21,15 @@ function PasswordTextField({
 ) {
   return (
     <TextField
+      name={name}
       label={label}
       placeholder={placeholder}
+      error={props.error}
+      helperText={props.helperText}
       type={'password'}
       value={props.value}
-      onChange={e => props.onValueChange(e.target.value)} />
+      onBlur={props.onBlur}
+      onChange={props.onChange} />
   );
 }
 
