5233fbab937b716983486b4572f8e884de4c9de1
Cris <filipcristimihail@yahoo.com>
2024-06-20 14:26:00 +0200

    #34 Projects - Add project Order Query Params enum

---
+++ src/features/projects/utils/ProjectOrderQueryParams.ts
@@ -0,0 +1,8 @@
+enum ProjectOrderQueryParams {
+  LastActivity = 'last_activity',
+  Name = 'name',
+  CreatedDateTime = 'created_date_time',
+  UpdatedDateTime = 'updated_date_time'
+}
+
+export default ProjectOrderQueryParams
