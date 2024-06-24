6afaf26dcd3637dd9e0a314f3c0e0d20bf2eef01
Cris <filipcristimihail@yahoo.com>
2024-06-24 18:17:46 +0200

    #33 Rich Tooltip - Width and delay

--- src/components/tooltip/RichTooltip.tsx
+++ src/components/tooltip/RichTooltip.tsx
@@ -18,25 +18,33 @@ const ThickDivider = styled(Divider, {
 
 interface RichTooltipProps {
   children: ReactElement,
-  title?: string | undefined,
-  arrow?: boolean | undefined,
+  title: string,
+  description?: string,
+  arrow?: boolean,
+  enterDelay?: number,
+  leaveDelay?: number,
+  width?: number,
   placement?: "bottom-end" | "bottom-start" | "bottom" | "left-end" | "left-start" | "left" | "right-end" | "right-start" | "right" | "top-end" | "top-start" | "top" | undefined,
-  description?: string | undefined,
 }
 
 function RichTooltip({
   children,
   title,
   description,
+  enterDelay = 300,
+  leaveDelay = 100,
   placement = 'top',
   arrow = true,
+  width = 150
 }: RichTooltipProps) {
   return (
     <Tooltip
       arrow={arrow}
+      enterDelay={enterDelay}
+      leaveDelay={leaveDelay}
       placement={placement}
       title={
-        <Stack mx={0.5} my={0.75} spacing={0.5}>
+        <Stack mx={0.3} my={0.5} spacing={0.5} width={width}>
           <Typography>{title}</Typography>
           <ThickDivider thickness={2} />
           <Typography variant={'subtitle1'}>{description}</Typography>
